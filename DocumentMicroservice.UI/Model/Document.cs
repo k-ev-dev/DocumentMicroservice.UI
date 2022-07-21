991088b77e564aaaec1d95bbd84a165471db2418
Evgenii Kolesnikov <koles.ikov@yandex.ru>
2022-07-21 11:56:30 +0300

    Добавьте файлы проекта.

---
+++ DocumentMicroservice.UI/Model/Document.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DocumentMicroservice.UI.Model {
+    public class Document {
+        public Guid Id { get; set; }
+        // TODO: добавить атрибуты
+        public string Title { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+    }
+}
